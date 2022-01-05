cc1c14ddf40470ae46eb5f6a4c1f5aafe95f92e0
HanInJu <haninju95@gmail.com>
2022-01-06 00:35:11 +0900

    여러 yml 파일을 읽을 수 있도록 메인클래스 변경

--- src/main/java/com/devko/magnet/MagnetApplication.java
+++ src/main/java/com/devko/magnet/MagnetApplication.java
@@ -1,13 +1,19 @@
 package com.devko.magnet;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class MagnetApplication {
 
+    public static final String CONFIG_LOCATIONS = "spring.config.location=" + "classpath:application.yml, " + "classpath:aws.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(MagnetApplication.class, args);
+
+        new SpringApplicationBuilder(MagnetApplication.class)
+                .properties(CONFIG_LOCATIONS)
+                .run(args);
+
     }
 
 }
