f27390802ffdd0a137bbadb1ef367bbf6014697a
HanInJu <haninju95@gmail.com>
2022-01-12 22:21:33 +0900

    deploy : jenkins 4

--- src/main/java/com/devko/magnet/controller/HomeController.java
+++ src/main/java/com/devko/magnet/controller/HomeController.java
@@ -26,6 +26,6 @@ public class HomeController {
 
     @GetMapping
     public ResponseEntity executeTest() {
-        return new ResponseEntity("Hello, World! (ver.Jenkins3)", HttpStatus.OK);
+        return new ResponseEntity("Hello, World! (ver.Jenkins4)", HttpStatus.OK);
     }
 }
